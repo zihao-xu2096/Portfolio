d6d70b5d98fb4affeedf53b33fd472e604f3921e
Harry Xu <harryxcz@gmail.com>
2024-11-06 01:12:00 -0500

    Add work experience timeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css';
 import Contact from './sections/Contact/Contact';
+import Timeline from './sections/Experience/Timeline';
 import Footer from './sections/Footer/Footer';
 import Hero from './sections/Hero/Hero';
 import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
 
+
 function App() {
   return (
     <>
       <Hero />
+      <Timeline />
       <Projects />
       <Skills />
       <Contact />
